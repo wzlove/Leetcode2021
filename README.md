6a0632fe81302b7559fc970c581f8569c4e65501
zhanghaojie <zhanghaojie@xiaoniangao.com>
2021-04-29 13:08:34 +0800

    ADD: 新建项目

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Leetcode2021
 一起刷题小分队，坚持就是胜利
+
+## LeetCode 精选 TOP 面试题
+https://leetcode-cn.com/problem-list/2ckc81c/
---
+++ week01/day01.go
@@ -0,0 +1,8 @@
+package main
+
+/*
+	https://leetcode-cn.com/problems/two-sum/
+*/
+func twoSum(nums []int, target int) []int {
+	return nil
+}
