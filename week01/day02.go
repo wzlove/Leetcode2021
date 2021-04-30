950d84e5ab3b13361c99dadc44d7607b4b550c01
whoops <walterlionking@163.com>
2021-04-30 18:43:49 +0800

    迭代的方法实现

--- week01/day02.go
+++ week01/day02.go
@@ -9,6 +9,25 @@ type ListNode struct {
 	Next *ListNode
 }
 
+/**
+ * @Description: 指针遍历
+ */
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	return nil
+	result := new(ListNode)
+	curr := result
+	addNum := 0
+	for l1 != nil || l2 != nil || addNum > 0 {
+		if l1 != nil {
+			addNum += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			addNum += l2.Val
+			l2 = l2.Next
+		}
+		curr.Next = &ListNode{Val: addNum % 10}
+		addNum /= 10
+		curr = curr.Next
+	}
+	return result.Next
 }
